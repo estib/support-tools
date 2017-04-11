1feb1ffdb7f1faffd5526cf1e3f9959ba1210b2a
stephenlechner <stephen.lechner@datadoghq.com>
2017-04-11 18:34:37 -0400

    bugfix, add tracing in comments

--- open_trelloed_tickets.py
+++ open_trelloed_tickets.py
@@ -4,8 +4,11 @@ import json
 import time
 import datetime
 import argparse
+import authenticate
+# from ddtrace import tracer
 
 
+# @tracer.wrap(service='trello-update')
 def getLastTrelloUpdateDate(url, key, token):
     try:
         the_text = requests.get(
@@ -22,9 +25,11 @@ def getLastTrelloUpdateDate(url, key, token):
         )
         return ts
     except Exception:
-        raise "failed to get trello update timestamp, error: %s" % (Exception,)
+        print "failed to get trello update timestamp."
+        raise Exception
 
 
+# @tracer.wrap(service='trello-update')
 def getTrelloTickets(view_id, auth_email, auth_pass):
     fr_id = '24332916'
     trello_field_ids = [24335386, 24519783, 24560146]
@@ -36,9 +41,10 @@ def getTrelloTickets(view_id, auth_email, auth_pass):
     try:
         tics = resp['tickets']
     except KeyError:
-        raise "no 'tickets' object in view JSON reponse. Probably the result of a bad API request. Error: %s" % (KeyError,)
+        print "no 'tickets' object in view JSON response. Probably the result of a bad API request."
+        raise KeyError
 
-    if len(tics) > 1:
+    if len(tics) < 1:
         print "no tickets in view. ending."
         exit()
     tics_with_trel_links = []
@@ -67,6 +73,7 @@ def getTrelloTickets(view_id, auth_email, auth_pass):
     return tics_with_trel_links
 
 
+# @tracer.wrap(service='trello-update')
 def openTickets(ticket_list, auth_email, auth_pass):
     open_tickets_url = str(
         'https://datadog.zendesk.com/api/v2/tickets/update_many.json?ids=%s' %
@@ -84,39 +91,51 @@ def openTickets(ticket_list, auth_email, auth_pass):
     return resp
 
 
-parser = argparse.ArgumentParser(description='Script to iterate through a Zendesk View to open any tickets that have trello card updates')
-parser.add_argument("-v", "--view_id", type=str, required=True, help='Zendesk view id nubmer. Get this from your view URL')
-parser.add_argument("-e", "--email", type=str, required=True, help='Email of zendesk user')
-parser.add_argument("-p", "--password", type=str, required=True, help='Password of zendesk user')
-parser.add_argument("-m", "--max", type=str, help='max number of tickets to open. Default of 10. Limit of 100')
-parser.add_argument("-t", "--trello_token", required=True, type=str, help='trello authentication token')
-parser.add_argument("-a", "--trello_api_key", required=True, type=str, help='trello api key')
+# @tracer.wrap(service='trello-update')
+def main_script():
+    parser = argparse.ArgumentParser(description='Script to iterate through a Zendesk View to open any tickets that have trello card updates')
+    parser.add_argument("-v", "--view_id", type=str, required=True, help='REQUIRED: Zendesk view id nubmer. Get this from your view URL\n')
+    parser.add_argument("-m", "--max", type=int, default=10, help='USEFUL: Max number of tickets to open. Default of 10. Limit of 100\n')
+    parser.add_argument("-e", "--email", type=str, help='OPTIONAL: Override for email of zendesk user\n')
+    parser.add_argument("-p", "--password", type=str, help='OPTIONAL: Override for password of zendesk user\n')
+    parser.add_argument("-t", "--trello_token", type=str, help='OPTIONAL: Override for trello authentication token\n')
+    parser.add_argument("-a", "--trello_api_key", type=str, help='OPTIONAL: Override for trello api key\n')
+    parser.add_argument("--verbose", action='store_true', help='DEBUG: Add for extra info on what\'s happening in the script.\n')
 
-uargs = parser.parse_args()
+    uargs = parser.parse_args()
+    view_id = uargs.view_id
+    email = uargs.email or authenticate.zendesk_email
+    password = uargs.password or authenticate.zendesk_password
+    t_api = uargs.trello_api_key or authenticate.trello_api_key
+    t_tok = uargs.trello_token or authenticate.trello_api_token
+    v = uargs.verbose
 
-trello_tics = getTrelloTickets(uargs.view_id, uargs.email, uargs.password)
+    trello_tics = getTrelloTickets(view_id, email, password)
+    if v:
+        print "tickets to check: \n%s" % (trello_tics,)
+    # compare times
+    tickets_to_open = []
+    cap = uargs.max
+    cnt = 1
+    for tic in trello_tics:
+        added = False
+        if cnt <= cap:
+            for link in tic['trello_links']:
+                trello_time = getLastTrelloUpdateDate(link, t_api, t_tok)
+                if trello_time >= tic['ticket_updated'] and added is False:
+                    if v:
+                        print tic['ticket_id'], tic['ticket_updated'], trello_time
+                    tickets_to_open += [tic['ticket_id']]
+                    added = True
+                    cnt += 1
+    if v:
+        print "tickets needing opening:", str(len(tickets_to_open))
+        print tickets_to_open
+    if len(tickets_to_open) > 0:
+        confirm = openTickets(tickets_to_open, email, password)
+        print confirm
+    else:
+        print "no tickets require opening."
 
-# compare times
-tickets_to_open = []
-cap = uargs.max or 10
-cnt = 1
-for tic in trello_tics:
-    added = False
-    if cnt <= cap:
-        for link in tic['trello_links']:
-            trello_time = getLastTrelloUpdateDate(
-                link, uargs.trello_api_key, uargs.trello_token
-            )
-            if trello_time >= tic['ticket_updated'] and added is False:
-                # print tic['ticket_id'], tic['ticket_updated'], trello_time
-                tickets_to_open += [tic['ticket_id']]
-                added = True
-                cnt += 1
 
-# print "tickets needing opening:", str(len(tickets_to_open))
-print tickets_to_open
-if len(tickets_to_open) > 0:
-    confirm = openTickets(tickets_to_open, uargs.email, uargs.password)
-    print confirm
-else:
-    print "no tickets require opening."
+main_script()
